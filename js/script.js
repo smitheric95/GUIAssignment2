b1f6fafb1c8a0acf89ccc1afea70e39799502693
smitheric95 <ericsmith@Sids-iMac.local>
2016-10-01 22:57:12 -0500

    color

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	var sq = "<div class='square invisiSquare'></div>";
 	var sqCount = 30;
 
-	for(var i = 1; i <= sqCount; i++){
+	for(var i = 0; i < sqCount; i++){
 		var color = 51 * (i%5);
 
 		sec.append( 
 			$(sq).css({
-				'background-color' : 'rgb('+color+','+color+','+color+')'
+				'background-color' : 'rgb('+color+','+color*3+','+color*4+')'
 			})
 		);
 	}
