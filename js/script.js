73a9e7e5387c96cf8608324373d1897d41004920
smitheric95 <ericsmith@Sids-iMac.local>
2016-10-01 21:53:43 -0500

    code reduction

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-	var sq = $('.square');
 	var sec = $('section');
-
+	var sq = $('.square')[0].outerHTML;
+	
 	for(var i=0;i<29;i++){
-		sec.append( $(sq[0].outerHTML).addClass('invisiSquare') );
+		sec.append( $(sq).addClass('invisiSquare') );
 	}
 
 	$('button').click(function(){
 		$('.square').each(function(){
 			$(this).removeClass('invisiSquare');
-		});
-
-		$('.square').click(function(){
+		}).click(function(){
 			$(this).addClass('invisiSquare');
 		});
 	});
+
 });
 
