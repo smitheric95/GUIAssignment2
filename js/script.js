a5788f0953f9d776fb9ab7234f7e6349d6bdedff
smitheric95 <ericsmith@Sids-iMac.local>
2016-10-01 22:54:09 -0500

    ok, now im done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 	var sec = $('section');
-	var sq = $('.square')[0].outerHTML;
+	var sq = "<div class='square invisiSquare'></div>";
 	var sqCount = 30;
 
-	for(var i = 2; i <= sqCount; i++){
+	for(var i = 1; i <= sqCount; i++){
 		var color = 51 * (i%5);
-		console.log(color);
 
 		sec.append( 
-			$(sq).addClass('invisiSquare').css({
+			$(sq).css({
 				'background-color' : 'rgb('+color+','+color+','+color+')'
 			})
 		);
